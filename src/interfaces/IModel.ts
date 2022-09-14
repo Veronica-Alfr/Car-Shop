88f08bd660f9a1adf496a9b83dfcd8ab3d01c533
Veronica-Alfr <veagalves@gmail.com>
2022-09-14 16:01:09 -0300

    fix: retirado string do metodo create

--- src/interfaces/IModel.ts
+++ src/interfaces/IModel.ts
@@ -1,5 +1,5 @@
 interface IModel<T> {
-  create(obj:T):Promise<T & { _id: string }>,
+  create(obj:T):Promise<T>,
   read():Promise<T[]>,
   readOne(_id:string):Promise<T | null>,
   update(_id:string, obj:T):Promise<T & { _id: string } | null>,
