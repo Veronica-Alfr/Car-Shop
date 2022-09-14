05a358b3f8aad23c9c7c2b380f66aaa9472e04c3
Veronica-Alfr <veagalves@gmail.com>
2022-09-14 18:35:45 -0300

    fix: Strings must use singlequote and line broke after int()

--- src/interfaces/IVehicle.ts
+++ src/interfaces/IVehicle.ts
@@ -4,18 +4,21 @@ const VehicleZodSchema = z.object({
   model: z.string({
     required_error: 'Model is required',
     invalid_type_error: 'Model must be a string',
-    }).min(3, { message: 'Model must be 3 or more characters long' }),
+  }).min(3, { message: 'Model must be 3 or more characters long' }),
   year: z.number({
     required_error: 'Year is required',
     invalid_type_error: 'Year must be a number',
-  }).int().positive().gte(1900).lte(2022),
+  }).int()
+    .positive()
+    .gte(1900)
+    .lte(2022),
   color: z.string({
     required_error: 'Color is required',
     invalid_type_error: 'Color must be a string',
   }).min(3, { message: 'Color must be 3 or more characters long' }),
   status: z.boolean({
-    required_error: "Status is required",
-    invalid_type_error: "Status must be a boolean",
+    required_error: 'Status is required',
+    invalid_type_error: 'Status must be a boolean',
   }).optional(),
   buyValue: z.number({
     required_error: 'Buy value (buyValue) is required',
