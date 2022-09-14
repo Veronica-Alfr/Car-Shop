a298ed2aa2559abddf1ed58df1401bac9adafcbf
Veronica-Alfr <veagalves@gmail.com>
2022-09-14 15:17:55 -0300

    fix: indentation of 2 spaces but found 4 and unnecessary semicolon

--- src/interfaces/IModel.ts
+++ src/interfaces/IModel.ts
@@ -1,9 +1,9 @@
 interface IModel<T> {
-    create(obj:T):Promise<T & { _id: string }>,
-    read():Promise<T[]>,
-    readOne(_id:string):Promise<T | null>,
-    update(_id:string, obj:T):Promise<T & { _id: string } | null>,
-    delete(_id:string):Promise<T | null>,
-};
+  create(obj:T):Promise<T & { _id: string }>,
+  read():Promise<T[]>,
+  readOne(_id:string):Promise<T | null>,
+  update(_id:string, obj:T):Promise<T & { _id: string } | null>,
+  delete(_id:string):Promise<T | null>,
+}
   
 export default IModel;
