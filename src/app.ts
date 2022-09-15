88742a66c951d020e817230dd692793f7cb3aadf
Veronica-Alfr <veagalves@gmail.com>
2022-09-15 10:00:02 -0300

    fix: rota /cars foi add

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,8 @@ import errorHandler from './middlewares/error';
 const app = express();
 app.use(express.json());
 
-app.use(carsRoute);
+app.use('/cars', carsRoute);
+
 app.use(errorHandler);
 
 export default app;
